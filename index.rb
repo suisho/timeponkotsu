3773ab0fbcfc685af91aa603c4d5840f484e5879
suisho <suisho000000@gmail.com>
2014-02-26 22:16:33 +0900

    Date使うやつ

--- index.rb
+++ index.rb
@@ -2,3 +2,4 @@ require './time'
 
 p Date.strptime("1500-10-10").strftime("%Y-%m-%d")
 p Date.strptime_with_mock_date("1500-10-10").strftime("%Y-%m-%d")
+p Date.strptime_with_mock_date2("1500-10-10").strftime("%Y-%m-%d")
--- time.rb
+++ time.rb
@@ -16,6 +16,16 @@ class Date #:nodoc:
       Time.strptime(str, fmt).to_date
     end
 
+    def strptime_with_mock_date2(str = '-4712-01-01', fmt = '%F', start = Date::ITALY)
+      unless start == Date::ITALY
+        raise ArgumentError, "Timecop's #{self}::#{__method__} only " +
+                             "supports Date::ITALY for the start argument."
+      end
+
+      Date.strptime(str, fmt).to_date
+    end
+
+
     #alias_method :strptime, :strptime_with_mock_date
   end
 end
